7b17017c97736b6e0869a418b251a2652a0c9cd2
Jakob Geiger <jakobgeiger15@gmail.com>
2022-01-12 21:48:10 -0500

    commit tree fdbebb31e15685619b1e38c910ce08584183e540

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return Math.abs([someValue - 42] * 264) 
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distance = Math.abs([start - destination] * 264)
+    return distance
+
+}
+
+function calculatesFarePrice(start, destination) {
+    Math.abs([start - destination] * 264)
+    let distance = Math.abs([start - destination] * 264)
+
+    if (distance <= 400) {  
+        return 0;
+    }
+    else if (distance > 400 && distance <= 2000) {
+        return 2.56;
+    }
+    else if (distance > 2000 && distance <= 2500) {
+        return 25
+    }
+    else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+    
+}
